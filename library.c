12880a22cbd83b2ba66da55d0e8b074f4ff8335a
SoMZeY <vvbordia@gmail.com>
2025-01-29 13:21:22 -0500

    Created the base structure

---
+++ library.c
@@ -0,0 +1,16 @@
+#include "graphics.h"
+
+void init_graphics()
+{
+
+}
+
+void exit_graphics() {}
+char getkey() {}
+void sleep_ms(long ms) {}
+void clear_screen(void *img) {}
+void draw_pixel(void *img, int x, int y, color_t color) {}
+void draw_line(void *img, int x1, int y1, int x2, int y2, color_t c) {}
+void fill_triangle(void *img, int x1, int y1, int x2, int y2, int x3, int y3, color_t c) {}
+void *new_offscreen_buffer() {}
+void blit(void *src) {}
